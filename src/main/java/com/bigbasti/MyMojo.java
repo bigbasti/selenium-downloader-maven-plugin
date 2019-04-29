ab30871b0c7d146ed5404671c52c5b2c5f7019e2
Sebastian Gross <s.gross@t-systems.com>
2019-04-29 22:39:45 +0200

    First draft of the planned plugin configuration

--- src/main/java/com/bigbasti/MyMojo.java
+++ src/main/java/com/bigbasti/MyMojo.java
@@ -1,38 +1,55 @@
 package com.bigbasti;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**
- * Goal which touches a timestamp file.
- *
- * @goal touch
- * 
- * @phase process-sources
- */
-public class MyMojo
-    extends AbstractMojo
+@Mojo(name = "selenium", defaultPhase = LifecyclePhase.TEST_COMPILE)
+public class MyMojo extends AbstractMojo
 {
+    /*
+        <configuration>
+            <useProxy>default|system|maven</useProxy> [default: maven][optional]
+            <!-- if there is more than one os specified in the downloads section, only download file for the current os -->
+            <!-- this speeds up the build process as fewer files must be downloaded -->
+            <downloadOnlyCurrentOs>true</downloadOnlyCurrentOs> [default:false][optional]
+            <!-- if there is already a file with a matching name at the target location skip downloading -->
+            <overwriteExisting>false</overwriteExisting> [default: false][optional]
+            <!-- the directory to save the downloaded zip files containing the drivers -->
+            <downloadTargetDirectory>${project.basedir}/src/test/resources/webdriver/zipped</downloadTargetDirectory> [default: ${project.basedir}/src/test/resources/webdriver/zipped][optional]
+            <!-- the directory where the unzipped driver binaries should be copied to -->
+            <driverTargetDirectory>${project.basedir}/src/test/resources/webdriver/binaries</driverTargetDirectory>[default: ${project.basedir}/src/test/resources/webdriver/binaries][optional]
+            <downloadRetryAttempts>1</downloadRetryAttempts> [default: 1][optional]
+            <downloadConnectTimeout>10000</downloadConnectTimeout> [defaulr: 10000][optional]
+            <downloads>
+                <download>
+                    <os>windows</os>
+                    <driver>chrome</chrome>
+                    <version>2.46</version>
+                    <architecture>64</architecture>
+                    <url>https://chromedriver.storage.googleapis.com/2.46/chromedriver_win32.zip</url>
+                    <hash>a95696782d22d94bb60f83a315e8a584f2ff5db2</hash> [optional]
+                    <algorithm>sha1</algorithm> [optional]
+                </download>
+                <download>
+                    <os>linux</os>
+                    <driver>chrome</chrome>
+                    <version>2.46</version>
+                    <architecture>64</architecture>
+                    <url>https://chromedriver.storage.googleapis.com/2.46/chromedriver_linux64.zip</url>
+                    <hash>402a44dc7719f7d40d4493942e2bd3238fa99ce5</hash> [optional]
+                    <algorithm>sha1</algorithm> [optional]
+                </download>
+            </downloads>
+        </configuration>
+     */
+
+
     /**
      * Location of the file.
      * @parameter expression="${project.build.directory}"
